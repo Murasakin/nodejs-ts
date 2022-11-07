78170e9d51a349fca9a4dfdcfabc499b674b109d
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2022-11-07 16:07:10 -0300

    adicionados third party middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+import compression from 'compression';
 import express, { Request, Response } from 'express';
 import router from './api/routes';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+//compresses all the responses
+app.use(compression());
+//adding a set of security middlewares
+app.use(helmet());
+//parse incoming request body and append data to `req.body`
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//enable all CORS request
+app.use(cors());
 
 app.use('/api/', router);
 
