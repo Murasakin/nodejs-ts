437981aaa834748b7c99270fb5df32a09fb30502
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2022-11-14 13:47:02 -0300

    implementado CRUD básico

--- src/api/teams/teams.routes.ts
+++ src/api/teams/teams.routes.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
-import { getTeams, teamsErrorTest } from './teams.controller';
+import {
+  getTeams,
+  teamsErrorTest,
+  getTeamById,
+  addTeam,
+  updateTeamById,
+  deleteTeamById,
+} from './teams.controller';
 import * as Auth from './../middlewares/auth.middleware';
 
 export const router = Router();
 
 router.route('/').get(Auth.authorize(['getTeams']), getTeams);
 router.route('/error').get(Auth.authorize(['getTeams']), teamsErrorTest);
+router.route('/:id').get(Auth.authorize(['getTeams']), getTeamById);
+router.route('/').post(Auth.authorize(['addTeams']), addTeam);
+router.route('/:id').patch(Auth.authorize(['updateTeams']), updateTeamById);
+router.route('/:id').delete(Auth.authorize(['deleteTeams']), deleteTeamById);
